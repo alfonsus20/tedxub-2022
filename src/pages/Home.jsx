2d8ccc70dd418b390f1ab36d0947e73003020df4
alfonsus20 <alfonschandrawan@gmail.com>
2022-09-12 10:21:46 +0700

    update video config

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -94,7 +94,7 @@ const Home = () => {
           }}
         >
           <div className="w-[120%] absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 overflow-hidden flex justify-center items-center">
-            <video className="w-[85%] h-full mx-auto">
+            <video className="w-[85%] h-full mx-auto" autoPlay muted loop>
               <source src="https://api.tedxuniversitasbrawijaya.com/storage/video.mp4" />
             </video>
           </div>

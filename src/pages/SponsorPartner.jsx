0c4dbed9b2102e135f0ebd1d58b05b7e8748b2c9
argafrl <arga_muhammad@yahoo.co.id>
2022-09-20 02:16:46 +0700

    Adding Common Image Asset for Background

--- src/pages/SponsorPartner.jsx
+++ src/pages/SponsorPartner.jsx
@@ -62,12 +62,13 @@ const SponsorPartner = () => {
   }, []);
   
   return (
-    <div className="min-h-screen bg-main-1 w-full bg-cover bg-no-repeat" style={{ backgroundImage: `url("${background}")` }}>
+    <div className="min-h-screen bg-main-1 w-full bg-cover bg-no-repeat relative" style={{ backgroundImage: `url("${background}")` }}>
       <Spinner isOpenSpinner={isOpenSpinner} onCloseSpinner={handleCloseSpinner}/>
-      <img className="absolute -z-0 left-0 h-96" src={hand} alt="Hand" />
+      <img className="absolute -z-0 left-0 h-64 lg:h-96" src={hand} alt="Hand" />
+      <img className="absolute -z-0 right-0 bottom-36 h-64 lg:h-96 rotate-180" src={hand} alt="Hand" />
       <div className="container m-auto z-10">
 
-        <div className="flex flex-col flex-wrap justify-center items-center m-auto max-w-6xl z-50">
+        <div className="flex flex-col flex-wrap justify-center items-center m-auto max-w-6xl z-10">
           <div className="heading text-center mb-14 relative m-auto">
             <h1 className="font-sedgwick text-7xl lg:text-8xl text-main-3 opacity-75 z-0">Sponsor</h1>
             <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 w-full mt-2">
@@ -109,7 +110,7 @@ const SponsorPartner = () => {
           </div>
         </div>
 
-        <div className="flex flex-col flex-wrap justify-center items-center m-auto max-w-6xl">
+        <div className="flex flex-col flex-wrap justify-center items-center m-auto max-w-6xl z-10">
           <div className="heading text-center my-14 relative">
             <h1 className="font-sedgwick text-7xl lg:text-8xl text-main-3 opacity-75 z-0">Community Partner</h1>
             <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 w-full mt-2">
@@ -117,7 +118,7 @@ const SponsorPartner = () => {
               <p className="font-sedgwick text-main-2 w-full">of TEDxUniversitasBrawijaya</p>
             </div>
           </div>
-          <div className="flex flex-row items-center justify-center gap-5 lg:gap-10 flex-wrap m-auto">
+          <div className="flex flex-row items-center justify-center gap-5 lg:gap-10 flex-wrap m-auto z-10">
             {allData.communityPartners.map((communityPartner) => (
               <div key={communityPartner.id} className="w-48 h-48 bg-white flex justify-center items-center rounded-xl overflow-hidden">
                 <img

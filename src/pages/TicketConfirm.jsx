b5c52b296f01d68072bf1b858fd05b67c8b40da1
argafrl <arga_muhammad@yahoo.co.id>
2022-09-22 07:49:04 +0700

    Adding response message status

--- src/pages/TicketConfirm.jsx
+++ src/pages/TicketConfirm.jsx
@@ -74,7 +74,7 @@ const TicketConfirm = () => {
         }
       )
       
-      if( data.message == "Success create invoice" ){
+      if( data.message == "Success create invoice" || data.message == "There is still pending transaction"){
         return window.location.replace(data.data.invoice_url);
       }
     } catch (error) {

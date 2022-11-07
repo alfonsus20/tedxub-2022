d030e8cd201963da41181cb6becec6133b4533f2
argafrl <arga_muhammad@yahoo.co.id>
2022-11-07 19:42:20 +0700

    Fixing Sponsor Logo Sizing and Pre Production Workflows API Link

--- src/pages/SponsorPartner.jsx
+++ src/pages/SponsorPartner.jsx
@@ -81,7 +81,7 @@ const SponsorPartner = () => {
               <div key={sponsor.id} className="w-48 h-48 bg-white flex justify-center items-center rounded-xl overflow-hidden">
                 <img
                   src={sponsor.link_foto_sponsor}
-                  className="w-full"
+                  className="max-h-48 object-cover object-center"
                   alt={sponsor.nama}
                 />
               </div>
@@ -89,7 +89,7 @@ const SponsorPartner = () => {
           </div>
         </div>
 
-        <div className="flex flex-col flex-wrap justify-center items-center m-auto max-w-6xl">
+        <div className="flex flex-col flex-wrap justify-center items-center m-auto max-w-6xl z-10">
           <div className="heading text-center my-14 relative">
             <h1 className="font-sedgwick text-7xl lg:text-8xl text-main-3 opacity-75 z-0">Media Partner</h1>
             <div className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 w-full mt-2">
@@ -97,12 +97,12 @@ const SponsorPartner = () => {
               <p className="font-sedgwick text-main-2 w-full">of TEDxUniversitasBrawijaya</p>
             </div>
           </div>
-          <div className="flex flex-row items-center justify-center gap-5 lg:gap-10 flex-wrap m-auto">
+          <div className="flex flex-row items-center justify-center gap-5 lg:gap-10 flex-wrap m-auto z-10">
             {allData.mediaPartners.map((mediaPartner) => (
               <div key={mediaPartner.id} className="w-48 h-48 bg-white flex justify-center items-center rounded-xl overflow-hidden">
                 <img
                   src={mediaPartner.link_foto_sponsor}
-                  className="w-full"
+                  className="max-h-48 object-cover object-center"
                   alt={mediaPartner.nama}
                 />
               </div>
@@ -123,7 +123,7 @@ const SponsorPartner = () => {
               <div key={communityPartner.id} className="w-48 h-48 bg-white flex justify-center items-center rounded-xl overflow-hidden">
                 <img
                   src={communityPartner.link_foto_sponsor}
-                  className="w-full object-cover object-center"
+                  className="max-h-48 object-cover object-center"
                   alt={communityPartner.nama}
                 />
               </div>
